03ff31eac44484611612cc6330b1e89c975bfe00
Jasmine0201 <245694881@qq.com>
2024-04-28 16:40:25 +0800

    add some annotation

--- hardware_test/radiowave_serial.py
+++ hardware_test/radiowave_serial.py
@@ -1,3 +1,7 @@
+# 使用海邻科的毫米波模块LD6002及其测试底板（底板上包括TTL转usb模块）
+# 通过usb连接模块底板和电脑，可进行如下串口读取 
+# 注意，在实际项目中，需要将毫米波模块数据传至MCU, 再通过蓝牙模块传输数据到手机，最后通过http(s) API将数据传到后端处理
+
 import serial   #pip install pyserial
 
 ser=serial.Serial('COM7',1382400, timeout=0.05) #50ms更新数据
