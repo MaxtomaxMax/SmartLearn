f85069f1e1f319bd2c45a8c0717a5e81be9fa7cc
MaxtomaxMax <1648212669@qq.com>
2024-05-05 15:18:02 +0800

    前端页面+1

---
+++ software_test/mini_program_test/pages/login/init_loading.vue
@@ -0,0 +1,35 @@
+<!-- 刚刚开启小程序的加载页面 -->
+<template>
+	<view class="content">
+		<image class="logo_white" src="../../static/ui_icon/logo_white.png"></image>
+		<text class="logo_text">智学</text>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.content{
+		/* 让页面全部铺满 */
+		height: 100vh;	
+		width: 100vw;
+		background: #7452FF;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;	/*水平居中*/
+		align-items: center;		/*垂直居中*/
+	}
+	.logo_white{
+		height: 200rpx;
+		width: 200rpx;
+		justify-content: center;
+		align-items: center;
+	}
+	.logo_text{
+		color: #FFFFFF;
+		font-size: 8vw;		/* 优先考虑相对大小*/
+		/* font-family: kaiti;	 */
+		font-family: lisu; 
+	}
+</style>
