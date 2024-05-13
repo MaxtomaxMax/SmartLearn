341e176e0447b39e78c204576e9a527a18cf845d
yuzopt <yuz7157@gmail.com>
2024-05-13 20:06:04 +0800

    bluetooth_pagebluetooth_page

--- software_test/mini_program_test_zy/pages/learn/bt_connected.vue
+++ software_test/mini_program_test_zy/pages/learn/bt_connected.vue
@@ -1,46 +1,55 @@
-<!-- 蓝牙已连接 -->
 <template>
-<view class="flex-col page">
-  <view class="flex-col flex-1 group">
-    <text class="self-center text">设备已配对</text>
-    <view class="flex-col items-start self-stretch section">
-      <text class="font">READY</text>
-      <text class="mt-8 font">？</text>
+  <view class="flex-col page">
+    <view> </view>
+    <!-- 获取服务与特征值，监听蓝牙特征值变化获取数据交互 -->
+    <button class="self-center button" @click="getservice">获取设备服务</button>
+    <button class="self-center button" @click="getcharacteristics">
+      获取连接设备特征值
+    </button>
+    <button class="self-center button" @click="charIdchange">
+      监听蓝牙特征值变化
+    </button>
+
+    <view class="flex-col flex-1 group">
+      <text class="self-center text">设备已配对</text>
+      <view class="flex-col items-start self-stretch section">
+        <text class="font">READY</text>
+        <text class="mt-8 font text_1">？</text>
+      </view>
+      <text class="self-center text_2">点击开始计时</text>
+      <text class="self-center text_3">00:00:00</text>
     </view>
-    <text class="self-center text_2">点击开始计时</text>
-    <text class="self-center text_3">00:00:00</text>
+    <view class="flex-row tab-bar">
+      <view class="flex-col items-center section_1">
+        <image
+          class="image_2"
+          src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=c27e47588fc51f198750cf896df6d092.png"
+        />
+        <text class="font_2 text_4">温故</text>
+      </view>
+      <view class="flex-col items-center section_2">
+        <image
+          class="image_2"
+          src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=95939757903d5fea0ce0279fd67d3926.png"
+        />
+        <text class="mt-2 font_2 text_5">知新</text>
+      </view>
+      <view class="flex-col items-center section_4">
+        <image
+          class="image_2"
+          src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=199555151d0ab2676d0571f82052a737.png"
+        />
+        <text class="font_2 text_6">三省</text>
+      </view>
+      <view class="flex-col items-center section_3">
+        <image
+          class="image_2"
+          src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=086346215073334eefa1a267175b8d92.png"
+        />
+        <text class="mt-6 font_2">吾身</text>
+      </view>
+    </view>
   </view>
-  <view class="flex-row tab-bar">
-    <view class="flex-col items-center section_2">
-      <image
-        class="shrink-0 image_2"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=c27e47588fc51f198750cf896df6d092.png"
-      />
-      <text class="font_2 text_4">温故</text>
-    </view>
-    <view class="flex-col items-center section_2">
-      <image
-        class="shrink-0 image_2"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=95939757903d5fea0ce0279fd67d3926.png"
-      />
-      <text class="mt-2 font_2 text_5">知新</text>
-    </view>
-    <view class="flex-col items-center section_2">
-      <image
-        class="shrink-0 image_2"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=199555151d0ab2676d0571f82052a737.png"
-      />
-      <text class="font_2 text_6">三省</text>
-    </view>
-    <view class="flex-col items-center section_3">
-      <image
-        class="shrink-0 image_2"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=086346215073334eefa1a267175b8d92.png"
-      />
-      <text class="mt-6 font_2">吾身</text>
-    </view>
-  </view>
-</view>
 </template>
 
 <script>
@@ -48,10 +57,158 @@ export default {
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      connectedDeviceId: "",
+      serviceId: "", // 假设你已知需要监听的服务ID
+      services: [],
+      characteristics: [],
+      characteristicId: "", // 假设你已知需要监听的特征ID
+    };
+  },
+  onLoad(options) {
+    if (options.deviceId) {
+      this.connectedDeviceId = options.deviceId;
+      uni.showModal({
+        title: "蓝牙已连接:",
+        content: `连接蓝牙设备ID：${this.connectedDeviceId}`,
+        showCancel: false,
+      });
+    } else {
+      console.error("No device ID passed to page");
+      uni.showModal({
+        title: "蓝牙未连接成功:",
+        content: "未接收到设备ID，无法连接蓝牙设备",
+        showCancel: false,
+      });
+    }
   },
 
-  methods: {},
+  methods: {
+    //获取设备服务,服务列表存放在services数组中
+    getservice() {
+      const that = this;
+      uni.getBLEDeviceServices({
+        deviceId: that.connectedDeviceId, // 在 uni-app 中通常不需要使用 `data` 属性
+        success: function (res) {
+          console.log(res.services);
+          let serviceIds = res.services.map((service) => service.uuid);
+          console.log(serviceIds);
+          that.services = res.services; // 直接更新响应式数据
+          that.info = "获取服务成功！===" + JSON.stringify(res.services);
+          uni.showModal({
+            title: "获取服务成功:",
+            content: `服务列表：${JSON.stringify(that.services)}`,
+            showCancel: false,
+          });
+        },
+        fail: function (err) {
+          that.info = "获取设备服务失败！" + err.message; // `errMsg` 变成 `message` 在某些平台
+        },
+      });
+    },
+    //获取特征值
+    getcharacteristics() {
+      const that = this;
+      if (that.services.length > 0) {
+        let myuuid = that.services[0].uuid; //服务数组第一项服务的uuid
+        uni.getBLEDeviceCharacteristics({
+          deviceId: that.connectedDeviceId,
+          serviceId: myuuid,
+          success: function (res) {
+            console.log(res.characteristics);
+            that.characteristics = res.characteristics.map((chr) => ({
+              uuid: chr.uuid,
+              notify: chr.properties.notify,
+              write: chr.properties.write,
+              read: chr.properties.read,
+            }));
+            that.info =
+              "获取特征值成功: " + JSON.stringify(that.characteristics);
+            uni.showModal({
+              title: "获取特征值成功:",
+              content: `特征值列表：${JSON.stringify(that.characteristics)}`,
+              showCancel: false,
+            });
+          },
+          fail: function (err) {
+            that.info = "特征值获取失败" + err.message;
+            uni.showModal({
+              title: "获取特征值失败:",
+              content: `未能成功获取服务特征值！`,
+              showCancel: false,
+            });
+          },
+        });
+      } else {
+        that.info = "无可用服务";
+      }
+    },
+
+    //监听特征值变化
+    charIdchange() {
+      const that = this;
+
+      if (
+        !that.connectedDeviceId ||
+        !that.services.length ||
+        !that.characteristics
+      ) {
+        uni.showModal({
+          title: "错误",
+          content: "设备未连接或服务特征未准备好",
+          showCancel: false,
+        });
+        return;
+      }
+      const serviceId = that.services[0].uuid;
+      const characteristicId = that.characteristics[1].uuid;
+      uni.notifyBLECharacteristicValueChange({
+        state: true,
+        deviceId: this.connectedDeviceId, // 使用已保存的设备ID
+        serviceId: serviceId, // 假设你已经定义并初始化了 serviceId 数组
+        characteristicId: characteristicId, // 假设你已经定义并初始化了 characteristicId 数组
+        success: function (res) {
+          console.log("notifyBLECharacteristicValueChange success");
+          that.info = "成功";
+          uni.showModal({
+            title: "监听特征值变化:",
+            content: `成功设置监听特征值变化！`,
+            showCancel: false,
+          });
+        },
+        fail: function (err) {
+          that.info = "失败：" + err.message; // 注意uni-app在某些平台上错误对象的属性可能是message
+          uni.showModal({
+            title: "监听特征值变化:",
+            content: `未成功设置监听特征值变化！错误信息：${err.errMsg}`,
+            showCancel: false,
+          });
+        },
+      });
+
+      // 函数将 ArrayBuffer 转换为十六进制字符串
+      function ab2hex(buffer) {
+        var hexArr = Array.prototype.map.call(
+          new Uint8Array(buffer),
+          function (bit) {
+            return ("00" + bit.toString(16)).slice(-2);
+          }
+        );
+        return hexArr.join("");
+      }
+
+      // 监听特征值变化
+      uni.onBLECharacteristicValueChange(function (res) {
+        console.log("characteristic value changed:", ab2hex(res.value));
+        that.info = "接收到的数据：" + ab2hex(res.value);
+        uni.showModal({
+          title: "接收到数据:",
+          content: `16进制数据为：${ab2hex(res.value)}`,
+          showCancel: false,
+        });
+      });
+    },
+  },
 };
 </script>
 
@@ -65,8 +222,19 @@ export default {
   height: 100%;
 }
 .group {
-  padding: 212.5rpx 120.83rpx 237.5rpx 141.67rpx;
+  padding: 210.44rpx 120.52rpx 236.29rpx 141.98rpx;
   overflow-y: auto;
+}
+.button {
+  margin: 10px 0;
+  padding: 10px;
+  width: 200px; /* 固定宽度 */
+  height: 50px; /* 固定高度 */
+  background-color: #7451ff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  text-align: center;
 }
 .text {
   color: #000000;
@@ -77,8 +245,8 @@ export default {
   letter-spacing: 8.33rpx;
 }
 .section {
-  margin: 66.67rpx 45.83rpx 0 25rpx;
-  padding: 141.67rpx 54.17rpx 62.5rpx 70.83rpx;
+  margin: 65.29rpx 46.15rpx 0 24.69rpx;
+  padding: 140.5rpx 55.77rpx 60.67rpx 71.31rpx;
   background-image: linear-gradient(180deg, #ffb238 0%, #ffeacc 100%);
   box-shadow: 0rpx 8.33rpx 6.25rpx #0000002e;
   border-radius: 50%;
@@ -90,15 +258,18 @@ export default {
   font-weight: 600;
   color: #000000;
 }
+.text_1 {
+  text-align: center;
+}
 .text_2 {
-  margin-top: 58.33rpx;
+  margin-top: 57.42rpx;
   color: #979797;
   font-size: 33.33rpx;
   font-family: ABeeZee;
   line-height: 30.79rpx;
 }
 .text_3 {
-  margin-top: 141.67rpx;
+  margin-top: 142.67rpx;
   color: #000000;
   font-size: 100rpx;
   font-family: ABeeZee;
@@ -106,15 +277,15 @@ export default {
   letter-spacing: 8.33rpx;
 }
 .tab-bar {
-  padding: 35.42rpx 0;
+  padding: 35.42rpx 0 35.42rpx;
   background-color: #ffffff;
   border-radius: 58.33rpx;
   box-shadow: 0rpx 1.04rpx 0rpx #0000002e inset;
   backdrop-filter: blur(0rpx);
 }
-.section_2 {
+.section_1 {
+  padding: 16.75rpx 0 10.35rpx;
   flex: 1 1 188rpx;
-  padding: 16.67rpx 0 8.33rpx;
   background-color: #ffffff;
   border-radius: 58.33rpx;
   overflow: hidden;
@@ -124,8 +295,24 @@ export default {
   width: 54rpx;
   height: 54rpx;
 }
+.section_2 {
+  padding: 14.67rpx 0 10.27rpx;
+  flex: 1 1 188rpx;
+  background-color: #ffffff;
+  border-radius: 58.33rpx;
+  overflow: hidden;
+  height: 102.08rpx;
+}
+.section_4 {
+  padding: 16.75rpx 0 10.44rpx;
+  flex: 1 1 188rpx;
+  background-color: #ffffff;
+  border-radius: 58.33rpx;
+  overflow: hidden;
+  height: 102.08rpx;
+}
 .section_3 {
-  padding: 8.33rpx 0;
+  padding: 6.33rpx 0 10.38rpx;
   flex: 1 1 188rpx;
   background-color: #ffffff;
   border-radius: 58.33rpx;
--- software_test/mini_program_test_zy/pages/learn/wait4bt.vue
+++ software_test/mini_program_test_zy/pages/learn/wait4bt.vue
@@ -1,90 +1,70 @@
-<!-- 等待蓝牙链接页面 -->
-<!-- wait for bluetooth -->
-<!--
 <template>
-	<view class="learn_bg">
-		
-		<!-- <view class="text1_container">
-			<text class="text1">请连接蓝牙</text>
-		</view>
-		
-		<view class="connect_bt">
-			<image class="bt_icon" src="../../static/ui_icon/bluetooth.png"></image>
-		</view>
-		
-		<view class="text2_container">
-			<text class="text2">点击连接蓝牙</text>
-		</view>
-	</view>
-</template>
+  <view class="flex-col page">
+    <view class="flex-col flex-1 group">
+      <button class="self-center button" @click="openBluetoothAdapter">
+        打开蓝牙适配器
+      </button>
 
-<script>
-</script>
+      <!-- 搜索蓝牙设备按钮 -->
+      <button class="self-center button" @click="searchBluetoothDevices">
+        开始搜索蓝牙设备
+      </button>
 
-<style>
-	.bt_icon{
-		height: 300rpx;
-		width: 300rpx;
-	}
-	.connect_bt{
-		height: 100vh;
-		width: 100vw;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;	/*水平居中*/
-		align-items: center;		/*垂直居中*/
-	}
-	/* .text1_container{
-		display: flex;
-		flex-direction: column;
-		height: 25%;
-	} */
-</style>
--->
-<template>
-<view class="flex-col page">
-  <view class="flex-col flex-1 group">
-    <text class="self-center text">请链接蓝牙</text>
-    <view class="flex-col justify-start items-center self-center image-wrapper">
-      <image
-        class="image_2"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=a559292b52c6d5675a881601d876b37a.png"
-      />
-    </view>
-    <text class="self-center text_2">点击链接蓝牙</text>
-    <text class="self-center text_3">00:00:00</text>
-    <view class="flex-row equal-division section">
-      <view class="flex-col items-center section_2 equal-division-item">
+      <!-- 示例的第二个搜索按钮，可以按需删除或更改 -->
+      <button class="self-center button" @click="closeDiscovery">
+        结束搜索蓝牙设备
+      </button>
+
+      <view
+        class="flex-col justify-start items-center self-center image-wrapper"
+      >
         <image
-          class="shrink-0 image_3"
-          src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=3807e5ce846f1a122c99fa99f0f2ff32.png"
+          class="image_2"
+          src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=a559292b52c6d5675a881601d876b37a.png"
         />
-        <text class="font text_4">温故</text>
       </view>
-      <view class="flex-col items-center section_2 equal-division-item">
-        <image
-          class="shrink-0 image_4"
-          src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=447c9226d59ea4dd46e7f54fb27260b4.png"
-        />
-        <text class="font text_5">知新</text>
-      </view>
-      <view class="flex-col items-center section_2 equal-division-item">
-        <image
-          class="shrink-0 image_5"
-          src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=a28466b963e1bce1c140eaa66dfc8c13.png"
-        />
-        <text class="font text_6">三省</text>
-      </view>
-      <view class="flex-col items-center section_2 equal-division-item">
-        <image
-          class="shrink-0 image_3"
-          src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=248a963a79e2bce3d28e2766eede344f.png"
-        />
-        <text class="font mt-3">吾身</text>
+      <view class="blank"> </view>
+
+      <button class="self-center button" @click="connectBluetooth">
+        点击连接蓝牙
+      </button>
+      <scroll-view class="device-list" scroll-y="true" style="height: 300px">
+        <block v-for="(device, index) in devices" :key="device.deviceId">
+          <button @tap="connectTo(device.deviceId)">{{ device.name }}</button>
+        </block>
+      </scroll-view>
+      <view class="flex-row equal-division section">
+        <view class="flex-col items-center section_2 equal-division-item">
+          <image
+            class="shrink-0 image_3"
+            src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=3807e5ce846f1a122c99fa99f0f2ff32.png"
+          />
+          <text class="font text_4">温故</text>
+        </view>
+        <view class="flex-col items-center section_2 equal-division-item">
+          <image
+            class="shrink-0 image_4"
+            src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=447c9226d59ea4dd46e7f54fb27260b4.png"
+          />
+          <text class="font text_5">知新</text>
+        </view>
+        <view class="flex-col items-center section_2 equal-division-item">
+          <image
+            class="shrink-0 image_5"
+            src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=a28466b963e1bce1c140eaa66dfc8c13.png"
+          />
+          <text class="font text_6">三省</text>
+        </view>
+        <view class="flex-col items-center section_2 equal-division-item">
+          <image
+            class="shrink-0 image_3"
+            src="https://ide.code.fun/api/image?token=663e5372d578370011c704fb&name=248a963a79e2bce3d28e2766eede344f.png"
+          />
+          <text class="font mt-3">吾身</text>
+        </view>
       </view>
     </view>
   </view>
-</view>
 </template>
 
 <script>
@@ -92,10 +72,149 @@ export default {
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      info: "",
+      devices: [],
+      services: [],
+    };
   },
+  methods: {
+    //打开蓝牙适配器
+    openBluetoothAdapter() {
+      const that = this;
+      uni.openBluetoothAdapter({
+        success(res) {
+          console.log("初始化蓝牙适配器成功", JSON.stringify(res));
+          that.info = "初始化蓝牙适配器成功" + JSON.stringify(res);
+          uni.showModal({
+            title: "蓝牙适配情况",
+            content: "初始化蓝牙适配器成功",
+            showCancel: false,
+          });
+        },
+        fail(res) {
+          console.log("初始化蓝牙适配器失败", JSON.stringify(res));
+          that.info = "初始化蓝牙适配器失败" + JSON.stringify(res);
+          uni.showModal({
+            title: "蓝牙适配情况",
+            content: "蓝牙适配失败，请检查手机蓝牙和定位功能是否打开",
+            showCancel: false,
+          });
+        },
+        complete() {
+          console.log("初始化蓝牙适配器完成");
+        },
+      });
+    },
+    //搜索蓝牙设备
+    searchBluetoothDevices() {
+      const that = this;
 
-  methods: {},
+      uni.startBluetoothDevicesDiscovery({
+        success(res) {
+          console.log("开始搜索蓝牙设备", res);
+          that.info = "开始搜索蓝牙设备" + JSON.stringify(res);
+          uni.showModal({
+            title: "蓝牙搜索",
+            content: "已成功开启蓝牙搜索",
+            showCancel: false,
+          });
+        },
+        fail(res) {
+          console.log("搜索设备失败", JSON.stringify(res));
+          that.info = "搜索设备失败" + JSON.stringify(res);
+          uni.showModal({
+            title: "蓝牙搜索",
+            content: "未成功开启蓝牙搜索",
+            showCancel: false,
+          });
+        },
+      });
+    },
+    //结束蓝牙设备搜索
+    closeDiscovery() {
+      const that = this;
+      uni.stopBluetoothDevicesDiscovery({
+        success(res) {
+          console.log(res);
+          uni.showModal({
+            title: "蓝牙搜索",
+            content: "已成功关闭蓝牙搜索",
+            showCancel: false,
+          });
+        },
+        fail(res) {
+          console.log(res, "fail");
+          uni.showModal({
+            title: "蓝牙搜索",
+            content: "未成功关闭蓝牙搜索",
+            showCancel: false,
+          });
+        },
+      });
+    },
+    //点击连接蓝牙获取蓝牙设备信息
+    connectBluetooth() {
+      const that = this;
+      uni.getBluetoothDevices({
+        success(res) {
+          that.devices = res.devices.map((device) => ({
+            deviceId: device.deviceId,
+            name: device.name || "未命名设备",
+          }));
+          that.info = "搜索到的蓝牙数目：" + res.devices.length;
+          uni.showModal({
+            title: "搜索蓝牙数目:",
+            content: `找到 ${res.devices.length} 个蓝牙设备`,
+            showCancel: false,
+          }); // 移除多余的逗号
+        },
+        fail(err) {
+          uni.showModal({
+            title: "搜索蓝牙数目:",
+            content: "获取蓝牙设备列表失败",
+            showCancel: false,
+          }); // 此处增加结束括号
+        },
+      });
+    },
+    //点击设备连接蓝牙
+    connectTo(deviceId) {
+      const that = this;
+      uni.createBLEConnection({
+        deviceId: deviceId,
+        success(res) {
+          console.log("连接设备成功", res);
+          that.info = "已连接设备ID：" + deviceId;
+          uni.showModal({
+            title: "连接设备成功:",
+            content: `已连接设备ID：${deviceId}, 点击确认跳转`,
+            showCancel: false,
+            success: function (modalRes) {
+              if (modalRes.confirm) {
+                // 启动倒计时
+                setTimeout(function () {
+                  // 页面跳转
+                  uni.navigateTo({
+                    url: "/pages/learn/bt_connected?deviceId=" + deviceId, // 修改为你的目标页面路径
+                  });
+                }, 1000); // 倒计时3秒
+              }
+            },
+          });
+        },
+        fail(err) {
+          console.error("连接设备失败", err);
+          that.info = "连接失败";
+          uni.showModal({
+            title: "连接设备失败:",
+            content: `未能成功连接设备ID：${deviceId}`,
+            showCancel: false,
+          });
+        },
+      });
+    },
+  },
 };
 </script>
 
@@ -110,6 +229,21 @@ export default {
   overflow-y: auto;
   overflow-x: hidden;
   height: 100%;
+}
+.blank {
+  width: 200px; /* 固定宽度 */
+  height: 50px; /* 固定高度 */
+}
+.button {
+  margin: 10px 0;
+  padding: 10px;
+  width: 200px; /* 固定宽度 */
+  height: 50px; /* 固定高度 */
+  background-color: #7451ff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  text-align: center;
 }
 .group {
   padding-top: 220.83rpx;
@@ -133,8 +267,8 @@ export default {
   width: 416.67rpx;
 }
 .image_2 {
-  width: 185.42rpx;
-  height: 166.67rpx;
+  width: 165.42rpx;
+  height: 146.67rpx;
 }
 .text_2 {
   margin-top: 58.33rpx;
