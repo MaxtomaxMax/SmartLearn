07524e048b36d324060a8f43ef642b63ee8e40bf
MaxtomaxMax <1648212669@qq.com>
2024-05-19 21:30:01 +0800

    配置更改

--- software_test/mini_program_test/pages/user/user_main_page.vue
+++ software_test/mini_program_test/pages/user/user_main_page.vue
@@ -1,215 +1,201 @@
-<!-- 吾身-入口 -->
 <template>
-<view class="flex-col page">
-  <view class="flex-col flex-1 group_2">
-    <view class="flex-col justify-start relative header">
-      <view class="flex-row justify-between group">
-        <image
-          class="image_2"
-          src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=24f8cb1f241816c388d930864dda0c7a.png"
-        />
-        <image
-          class="image_3"
-          src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=76bd8d16aed8583c32db970875acfaa3.png"
-        />
-      </view>
-    </view>
-    <view class="flex-col group_18">
-      <view class="flex-row group_3">
-        <image
-          class="shrink-0 self-center image_4"
-          src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=2182e647b8a6ea71092fe15fb8980ccd.png"
-        />
-        <view class="ml-14 flex-col items-start flex-1 self-start group_4">
-          <text class="text">Lee</text>
-          <text class="font text_2 mt-9">Wit beyond measure is man’s greatest treasure</text>
+  <view class="flex-col page">
+    <view class="flex-col flex-1 group_2">
+      <view class="flex-col justify-start relative header">
+        <view class="flex-row justify-between group">
+          <image
+            class="image_2"
+            src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=24f8cb1f241816c388d930864dda0c7a.png"
+          />
+          <image
+            class="image_3"
+            src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=76bd8d16aed8583c32db970875acfaa3.png"
+          />
         </view>
       </view>
-      <view class="flex-col section">
-        <view class="flex-row justify-between items-center section_2">
+      <view class="flex-col group_18">
+        <view class="flex-row group_3">
+          <image
+            class="shrink-0 self-center image_4"
+            src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=2182e647b8a6ea71092fe15fb8980ccd.png"
+          />
+          <view class="ml-14 flex-col items-start flex-1 self-start group_4">
+            <text class="text">{{ nickname }}</text>
+            <text class="font text_2 mt-9">{{ signature }}</text>
+          </view>
+        </view>
+
+        <view class="flex-col section">
+          <!--<view class="flex-row justify-between items-center section_2">
+                        <view class="flex-row items-center">
+                            <image
+                                class="shrink-0 image_5"
+                                src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=7ef3ccd9929c972089b8a5d57be030b8.png"
+                            />
+                            <text class="font_2 text_3 ml-11">学习排行榜</text>
+                        </view>
+                        <view class="group_5">
+                            <text class="font text_5">第</text>
+                            <text class="font_3 text_4">2</text>
+                            <text class="font text_6">名</text>
+                        </view>
+                    </view>-->
+          <view class="flex-row justify-between items-center section_2">
+            <view class="flex-row items-center">
+              <image
+                class="shrink-0 image_5"
+                src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=c4fb0376d2d6e382037314edaf19e346.png"
+              />
+              <text class="font_2 text_7 ml-11">净学习时长</text>
+            </view>
+            <view class="group_6">
+              <text class="font_3 text_8">{{ learninghours }}</text>
+              <text class="font_4 text_10">小时</text>
+              <text class="font_3 text_9">{{ learningminutes }}</text>
+              <text class="font text_11">分钟</text>
+            </view>
+          </view>
+
+          <!--<view class="flex-row justify-between items-center section_2">
+                        <view class="flex-row items-center">
+                            <image
+		finishedcount:19,
+		studyingcount:17,
+		detectcount:526,
+		avestress:33,
+                                class="shrink-0 image_5"
+                                src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=77eebaf052db6d0fa4ff6bd543121640.png"
+                            />
+                            <text class="font_2 text_12 ml-11">勋章</text>
+                        </view>
+                        <view class="group_7">
+                            <text class="font_3 text_13">25</text>
+                            <text class="font text_14">枚勋章</text>
+                        </view>
+                    </view>-->
+        </view>
+        <view>
+          <text class="self-start font_33 text_31"
+            >BASELINE-MONITOR·平静检测</text
+          >
+          <view
+            class="mt-14 flex-row justify-between items-center self-stretch section_30"
+            @click="goTodetect"
+          >
+            <view class="flex-row items-center">
+              <view class="flex-row items-center">
+                <view class="section_31"></view>
+                <image
+                  class="image_34 ml-5"
+                  src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=456c1bf87981560e563e0ca30574dc71.png"
+                />
+              </view>
+              <view class="ml-18 flex-col items-center">
+                <text class="font_34 text_32">BASELINE-MONITOR</text>
+                <text class="mt-12 font_33">平静值检测</text>
+              </view>
+            </view>
+            <image
+              class="image_35"
+              src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=9efdfc28904bd019d621cdadf31e2692.png"
+            />
+          </view>
+        </view>
+      </view>
+
+      <view class="grid">
+        <view class="flex-col grid-item_1">
+          <view class="flex-row items-center self-stretch">
+            <image
+              class="image_6"
+              src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=e59a70c1e3080a67cd25840e2e3cf158.png"
+            />
+            <text class="ml-12 font_5 text_25">在学</text>
+          </view>
+          <view class="self-start group_8 mt-9">
+            <text class="font_4">{{ studyingcount }}</text>
+            <text class="font_6">
+              本在学
+              <br />
+            </text>
+            <text class="font_6">今年在学12本</text>
+          </view>
+        </view>
+        <view class="flex-col grid-item_1">
+          <view class="flex-row items-center self-stretch">
+            <image
+              class="image_6"
+              src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=6aa68ad6f689258b883452447311a124.png"
+            />
+            <text class="ml-12 font_5 text_15">学完</text>
+          </view>
+          <view class="self-start group_8 mt-9">
+            <text class="font_4">{{ finishedcount }}</text>
+            <text class="font_6">
+              本学完
+              <br />
+            </text>
+            <text class="font_6">今年学完5本</text>
+          </view>
+        </view>
+        <view class="flex-col grid-item">
+          <view class="flex-row items-center self-stretch">
+            <image
+              class="image_6"
+              src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=6a497b9441a91bddb96ee9a9c92e5858.png"
+            />
+            <text class="ml-12 font_5 text_26">答疑</text>
+          </view>
+          <view class="self-start group_1 mt-9">
+            <text class="font_4">2829</text>
+            <text class="font_6">
+              个答疑
+              <br />
+            </text>
+            <text class="font_6">留在了26本书上</text>
+          </view>
+        </view>
+        <view class="flex-col grid-item_1">
+          <view class="flex-row items-center self-stretch">
+            <image
+              class="image_6"
+              src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=6fede10a8dac1767fdff2d1922b8c5b1.png"
+            />
+            <text class="ml-12 font_5 text_1">复习</text>
+          </view>
+          <view class="self-start group_8 mt-9">
+            <text class="font_4">{{ reviewedcount }}</text>
+            <text class="font_6">
+              条已复习
+              <br />
+            </text>
+            <text class="font_6">{{ unreviewcount }}条未复习</text>
+          </view>
+        </view>
+      </view>
+      <view class="flex-col section_3">
+        <view class="flex-row justify-between items-center section_4">
           <view class="flex-row items-center">
             <image
               class="shrink-0 image_5"
-              src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=7ef3ccd9929c972089b8a5d57be030b8.png"
+              src="https://ide.code.fun/api/image?token=6647a0967a1eb60011e76379&name=51a5d645ce89bc89c1c7e9f8cdc370c5.png"
             />
-            <text class="font_2 text_3 ml-11">学习排行榜</text>
+            <text class="font_2 text_16 ml-11">监测报告</text>
           </view>
-          <view class="group_5">
-            <text class="font text_5">第</text>
-            <text class="font_3 text_4">2</text>
-            <text class="font text_6">名</text>
+          <view class="group_9">
+            <text class="font_4">已监测</text>
+            <text class="font_7">{{ detectcount }}</text>
+            <text class="font_4">
+              次
+              <br />
+            </text>
+            <text class="font_4">平均压力值</text>
+            <text class="font_7">{{ avestress }}%</text>
           </view>
-        </view>
-        <view class="flex-row justify-between items-center section_2">
-          <view class="flex-row items-center">
-            <image
-              class="shrink-0 image_5"
-              src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=c4fb0376d2d6e382037314edaf19e346.png"
-            />
-            <text class="font_2 text_7 ml-11">净学习时长</text>
-          </view>
-          <view class="group_6">
-            <text class="font_3 text_8">377</text>
-            <text class="font_4 text_10">小时</text>
-            <text class="font_3 text_9">14</text>
-            <text class="font text_11">分钟</text>
-          </view>
-        </view>
-        <view class="flex-row justify-between items-center section_2">
-          <view class="flex-row items-center">
-            <image
-              class="shrink-0 image_5"
-              src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=77eebaf052db6d0fa4ff6bd543121640.png"
-            />
-            <text class="font_2 text_12 ml-11">勋章</text>
-          </view>
-          <view class="group_7">
-            <text class="font_3 text_13">25</text>
-            <text class="font text_14">枚勋章</text>
-          </view>
-        </view>
-      </view>
-    </view>
-    <view class="grid">
-      <view class="flex-col grid-item_1">
-        <view class="flex-row items-center self-stretch">
-          <image
-            class="image_6"
-            src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=e59a70c1e3080a67cd25840e2e3cf158.png"
-          />
-          <text class="ml-12 font_5 text_25">在学</text>
-        </view>
-        <view class="self-start group_8 mt-9">
-          <text class="font_4">26</text>
-          <text class="font_6">
-            本在学
-            <br />
-          </text>
-          <text class="font_6">今年在学12本</text>
-        </view>
-      </view>
-      <view class="flex-col grid-item_1">
-        <view class="flex-row items-center self-stretch">
-          <image
-            class="image_6"
-            src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=6aa68ad6f689258b883452447311a124.png"
-          />
-          <text class="ml-12 font_5 text_15">学完</text>
-        </view>
-        <view class="self-start group_8 mt-9">
-          <text class="font_4">17</text>
-          <text class="font_6">
-            本学完
-            <br />
-          </text>
-          <text class="font_6">今年学完5本</text>
-        </view>
-      </view>
-      <view class="flex-col grid-item">
-        <view class="flex-row items-center self-stretch">
-          <image
-            class="image_6"
-            src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=6a497b9441a91bddb96ee9a9c92e5858.png"
-          />
-          <text class="ml-12 font_5 text_26">答疑</text>
-        </view>
-        <view class="self-start group_1 mt-9">
-          <text class="font_4">2829</text>
-          <text class="font_6">
-            个答疑
-            <br />
-          </text>
-          <text class="font_6">留在了26本书上</text>
-        </view>
-      </view>
-      <view class="flex-col grid-item_1">
-        <view class="flex-row items-center self-stretch">
-          <image
-            class="image_6"
-            src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=6fede10a8dac1767fdff2d1922b8c5b1.png"
-          />
-          <text class="ml-12 font_5 text_1">复习</text>
-        </view>
-        <view class="self-start group_8 mt-9">
-          <text class="font_4">784</text>
-          <text class="font_6">
-            条已复习
-            <br />
-          </text>
-          <text class="font_6">321条未复习</text>
-        </view>
-      </view>
-    </view>
-    <view class="flex-col section_3">
-      <view class="flex-row justify-between items-center section_4">
-        <view class="flex-row items-center">
-          <image
-            class="shrink-0 image_5"
-            src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=51a5d645ce89bc89c1c7e9f8cdc370c5.png"
-          />
-          <text class="font_2 text_16 ml-11">监测报告</text>
-        </view>
-        <view class="group_9">
-          <text class="font_4">已监测</text>
-          <text class="font_7">321</text>
-          <text class="font_4">
-            次
-            <br />
-          </text>
-          <text class="font_4">平均压力值</text>
-          <text class="font_7">56%</text>
-        </view>
-      </view>
-      <view class="flex-row justify-between items-center section_5">
-        <view class="flex-row items-center">
-          <image
-            class="shrink-0 image_5"
-            src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=2f6421c12ee1e6e94dd9322051aaeb3e.png"
-          />
-          <text class="font_2 text_17 ml-11">关注</text>
-        </view>
-        <view class="group_10">
-          <text class="font_7">5</text>
-          <text class="font_4">
-            人关注我
-            <br />
-          </text>
-          <text class="font_4">已关注2人</text>
         </view>
       </view>
     </view>
   </view>
-  <view class="flex-row justify-between tab-bar">
-    <view class="flex-col items-center group_11">
-      <image
-        class="image_7"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=c27e47588fc51f198750cf896df6d092.png"
-      />
-      <text class="mt-2 font_8 text_18">温故</text>
-    </view>
-    <view class="flex-col items-center group_12">
-      <image
-        class="image_7"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=302b9c13da61d12b84b17285da4b7b0d.png"
-      />
-      <text class="mt-2 font_8 text_19">知新</text>
-    </view>
-    <view class="flex-col items-center group_13">
-      <image
-        class="image_7"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=199555151d0ab2676d0571f82052a737.png"
-      />
-      <text class="mt-2 font_8 text_20">三省</text>
-    </view>
-    <view class="flex-col items-center group_14">
-      <image
-        class="image_7"
-        src="https://ide.code.fun/api/image?token=663911a4bba59d0011c27cd0&name=337055d22fa08fca3b409b06d011b8ab.png"
-      />
-      <text class="mt-2 font text_21">吾身</text>
-    </view>
-  </view>
-</view>
 </template>
 
 <script>
@@ -217,10 +203,42 @@ export default {
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      nickname: "",
+      signature: "",
+      learninghours: 5,
+      learningminutes: 26,
+      finishedcount: 19,
+      studyingcount: 17,
+      detectcount: 526,
+      avestress: 33,
+      reviewedcount: 333,
+      unreviewcount: 424,
+    };
+  },
+  onShow() {
+    const globalData = getApp().globalData;
+    this.nickname = globalData.nickname;
+    this.signature = globalData.signature;
   },
 
-  methods: {},
+  methods: {
+    ClicktoSetting() {
+      uni.navigateTo({
+        url: "/pages/user/settings", // 目标页面的路径
+      });
+    },
+    goToReportPage() {
+      uni.navigateTo({
+        url: "/pages/user/detectreport", // 目标页面的路径
+      });
+    },
+    goTodetect() {
+      uni.navigateTo({
+        url: "/pages/user/bslinedetect", // 目标页面的路径
+      });
+    },
+  },
 };
 </script>
 
@@ -237,7 +255,7 @@ export default {
   width: 100%;
   overflow-y: auto;
   overflow-x: hidden;
-  height: 100%;
+  height: 100vh;
 }
 .group_2 {
   padding-bottom: 77.08rpx;
@@ -512,5 +530,54 @@ export default {
 .text_21 {
   color: #7451ff;
   line-height: 19.42rpx;
+}
+.list {
+  margin: 27.08rpx 0 -95.83rpx;
+}
+.list-item {
+  padding: 33.33rpx 41.67rpx;
+  background-color: #ffffff;
+}
+.image_35 {
+  margin-right: 41.67rpx;
+  width: 18.75rpx;
+  height: 25rpx;
+}
+.list-item:first-child {
+  margin-top: 0;
+}
+.font_33 {
+  font-size: 20.83rpx;
+  font-family: PingFang SC;
+  line-height: 19.31rpx;
+  color: #000000;
+}
+.text_31 {
+  line-height: 19.29rpx;
+}
+.section_30 {
+  background-color: #f4f2fc;
+  border-radius: 8.33rpx;
+}
+.section_31 {
+  background-image: linear-gradient(180deg, #ff9328 0%, #7451ff 100%);
+  border-radius: 8.33rpx;
+  width: 6.25rpx;
+  height: 116.67rpx;
+}
+.image_34 {
+  filter: drop-shadow(0rpx 2.08rpx 4.17rpx #00000040);
+  width: 79.17rpx;
+  height: 75rpx;
+}
+.font_34 {
+  font-size: 25rpx;
+  font-family: PingFang SC;
+  line-height: 23.48rpx;
+  font-weight: 600;
+  color: #000000;
+}
+.text_32 {
+  line-height: 18.54rpx;
 }
 </style>
